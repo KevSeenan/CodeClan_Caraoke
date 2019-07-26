fca3ae2ebf69825c68180c61f6afe1a86a4f6a22
KevSeenan <kev.seenan@gmail.com>
2019-07-26 12:22:32 +0100

    Adds song has a name test

--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -1,6 +1,6 @@
-equire('minitest/autorun')
+require('minitest/autorun')
 require('minitest/rg')
-require_relative('../song.rb')
+require_relative('../songs.rb')
 
 class TestSong < Minitest::Test
 
@@ -9,7 +9,8 @@ class TestSong < Minitest::Test
     @song2 = Song.new("Sweet Child of Mine", "Guns 'n' Roses")
   end
 
-  def test_song_has_a_name()
+  def test_song_has_a_title()
+    assert_equal("Always", @song1.title)
   end
 
 end
