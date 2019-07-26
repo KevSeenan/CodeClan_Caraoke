c4aa1beaee2b11d269b93c694ec9ada69b3d1f72
KevSeenan <kev.seenan@gmail.com>
2019-07-26 13:02:25 +0100

    Adds room has a check in time test

--- rooms.rb
+++ rooms.rb
@@ -1,9 +1,11 @@
 class Rooms
 
-  attr_reader :name
+  attr_reader :name, :check_in
 
-  def initialize(name)
+  def initialize(name, check_in)
     @name = name
+    @check_in = check_in
+    # @check_out = check_out
   end
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -1,15 +1,28 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../rooms.rb')
+# require_relative('../guests.rb')
 
 class TestRooms < Minitest::Test
 
   def setup()
-    @room1 = Rooms.new("RockBlock")
+    @room1 = Rooms.new("RockBlock", 17.30)
+    # @guest1 = Guests.new("Jonny Cash", 20.30, 22.30)
+    # @guest2 = Guests.new("Willie Nelson", 20.30, 22.30)
+    # @guest3 = Guests.new("Waylon Jennings", 20.30, 22.30)
+    # @guest4 = Guests.new("Kris Kristofferson", 20.30, 22.30)
   end
 
   def test_room_has_a_name()
     assert_equal("RockBlock", @room1.name)
   end
+
+  def test_check_room_has_a_check_in_time()
+    assert_equal(17.30, @room1.check_in)
+  end
+  #
+  # def test_check_room_has_a_check_out_time()
+  #   assert_equal(19.30, @room1.check_out)
+  # end
 
 end
